fb9e6a940672ee80bccbdb02053d4c8870ea76ee
Angelica Corrales <angelikcorrales@gmail.com>
2020-12-05 11:01:04 -0500

    ok

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/', methods=['POST', 'GET'])
+def amortizacion():
+ if request.method == 'POST': 
+    pagetitle = 'Tabla Amortizacion:'
+    lista_amortizacion = [
+
+    ]
+  
+    valorprestado = int(request.form['valor'])
+    tasa_interes = int(request.form['interes'])
+    tiempo_limite = int(request.form['tiempo'])
+
+    amortizacion = round(valorprestado/tiempo_limite)
+    saldo = valorprestado
+    
+    for i in range(int(tiempo_limite)):
+        interes_generado = round((saldo * (tasa_interes/100)))
+        valorpagar = interes_generado + amortizacion
+        saldo = (saldo + interes_generado) - valorpagar
+        deudainicial = round(saldo + amortizacion)
+        if saldo < 0:
+           saldo = 0 
+        lista_amortizacion.append(
+            {
+                'Mes': int(i+1),
+                'Deuda Inicial $': float(deudainicial),
+                'Interes Generado $': float(interes_generado),
+                'Amortizacion $': float(amortizacion),
+                'Valor a pagar $': float(valorpagar),
+                'Deuda Final $': float(saldo)
+            }
+        )
+
+    return render_template(
+        'index.html',
+        mytitle=pagetitle,
+        lista_amortizacion=lista_amortizacion
+    )
+
 
 @app.route('/conversor/<int:pesos>')
 def conversor_pesos_dolar(pesos):
@@ -14,3 +51,4 @@ def conversor_pesos_dolar(pesos):
 
 if __name__ == '__main__':
    app.run()
+
